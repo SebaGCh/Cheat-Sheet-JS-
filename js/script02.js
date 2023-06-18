a96575ff65a3382ae7b1438431a9110fb7033f88
SebaGCh <seba.cuentas@gmail.com>
2023-06-18 17:51:42 -0400

    Se agregaron parseInt

---
+++ js/script02.js
@@ -0,0 +1,18 @@
+//MATH
+resultado = Math.PI //Nos entrega PI
+resultado = Math.round(2.1) //redondea, 0.5 para arriba y 0.4 hacia abajo
+resultado = Math.ceil(2.1) //redondea siempre hacia arriba
+resultado = Math.floor(2.9) //Redondea siempre hacia abajo
+resultado = Math.sqrt(49) //Raiz cuadrada del numero
+resultado = Math.abs(-500) //Entrega el valor absoluto (500)
+resultado = Math.pow(2,4) //Recibe 2 argumentos, base y potencia.
+resultado = Math.min(3,5,8,6,9,10) //Entrega el numero más pequeño
+resultado = Math.max(1,2,3,4,5,25) //Entrega el numero más alto
+resultado = Math.random() //Entrega un float aleatorio entre 0 y 1
+
+//Convertir un string a numero
+const num1 = "20"
+console.log(parseInt(num1))
+console.log(parseFloat(num1)) //Esto lo convierte a un float
+console.log(isInterger(num1)) //Nos sirve para saber si una variable es un numero
+
